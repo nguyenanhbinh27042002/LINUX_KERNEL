ba867f60a1bfc080b448d181f6e7bb57d8c3af40
nguyenanhbinh27042002 <anhbinhnguyen27042002@gmail.com>
2026-04-26 16:03:48 +0700

    clean code the script

---
+++ timerAdder.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+typedef struct TimerStr
+{
+	int hour;
+	int min;
+	int sec;
+}Timer;
+
+int main(void)
+{
+	Timer time;
+	do
+	{
+		printf("Enter the timer :\n");
+		scanf("%d %d %d",&time.hour,&time.min,&time.sec);
+	} while (time.hour < 0 || time.hour > 23 || time.min < 0 || time.min > 59 || time.sec < 0 || time.sec > 59); 
+	int temp = time.hour*3600 + time.min*60 + time.sec;
+	temp = (temp+1)%(24*3600);
+	time.hour = temp/3600;
+	time.min = (temp%3600)/60;
+	time.sec = (temp%3600)%60;
+
+	printf("Time at the clock :%02d %02d %02d\n",time.hour,time.min,time.sec);
+	return 0;
+}
