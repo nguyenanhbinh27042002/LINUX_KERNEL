d21dcc6c4c3b83d3150938510de036a96243622b
nguyenanhbinh27042002 <anhbinhnguyen27042002@gmail.com>
2026-04-26 11:35:08 +0700

    Add the file Timer at clock

--- timerAdder.c
+++ timerAdder.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-typedef struct Timer_Str
+typedef struct TimerStr
 {
 	int hour;
 	int min;
@@ -11,9 +11,20 @@ typedef struct Timer_Str
 int main(void)
 {
 	Timer time;
-	printf("Enter the Timer adder\n");
-	scanf("%d %d %d",&time.hour,&time.min,&time.sec);
+	for(int i=0;;i++)
+	{
+		do
+		{
+			printf("Enter the timer :\n");
+			scanf("%d %d %d",&time.hour,&time.min,&time.sec);
+		} while (time.hour < 0 || time.hour > 24 || time.min < 0 || time.min > 59 || time.sec < 0 || time.sec > 59); break ;
+	}
+	int temp = time.hour*3600 + time.min*60 + time.sec;
+	temp = (temp+1)%(24*3600);
+	time.hour = temp/3600;
+	time.min = (temp%3600)/60;
+	time.sec = (temp%3600)%60;
 
-
+	printf("Time at the clock :%02d %02d %02d\n",time.hour,time.min,time.sec);
 	return 0;
 }
