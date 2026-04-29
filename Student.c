31e9a3dc2b01f776be26dd0d984501b0e1524992
nguyenanhbinh27042002 <anhbinhnguyen27042002@gmail.com>
2026-04-29 20:04:08 +0700

    Clean code and check the logic in this script

--- Student.c
+++ Student.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 
 
-typedef struct Student_str
+typedef struct StudentStr
 {
-    char id[20]; 
-    char nameStudent[20];
-    float gpa;
+  char id[20];
+  char nameStudent[20];
+  float gpa;
 }Student;
 
 int main(void)
 {
+    int N; 
     Student student[20];
-    int N;
-    printf("Enter the number student \n");
+    printf("Enter the number\n");
     scanf("%d",&N);
-    for(int i=0;i<N;i++)
+    for(int i=0 ; i<N;i++)
     {
-        printf("Enter the string value id, name student , gpa\n");
-        scanf("%s %s %f",student[i].id, student[i].nameStudent, &student[i].gpa);
+        printf("Enter the information student\n");
+        scanf("%s %s %f",student[i].id, student[i].nameStudent,&student[i].gpa);
+    }
 
-    }
-    printf("\nStudent list\n");
+    printf("Student list \n");
     for(int i=0;i<N;i++)
     {
-        printf("%s %s %f\n",student[i].id,student[i].nameStudent,student[i].gpa);
+        printf("%s %s %.2f\n",student[i].id,student[i].nameStudent,student[i].gpa);
     }
-    
     return 0;
 }
