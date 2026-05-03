62400fbe0f8a5e576fd08ecc4b16077f29c69a37
nguyenanhbinh27042002 <anhbinhnguyen27042002@gmail.com>
2026-05-03 14:07:07 +0700

    Clean code

--- distance_Function.c
+++ distance_Function.c
@@ -8,14 +8,13 @@ typedef struct Point
 }Point;
 
 int main(void)
-{
+{   
     Point ptr1;
     Point ptr2;
-    printf("Enter the coordition of the point: such as x1, y1, x2, y2\n");
+    printf("Enter the point for cacalate the distance\n");
     scanf("%f %f %f %f",&ptr1.x,&ptr1.y,&ptr2.x,&ptr2.y);
 
-    float distance = sqrt(pow(ptr1.x-ptr2.x,2) + pow(ptr1.y-ptr2.y,2));
-    printf("%f %f %f %f\n",ptr1.x,ptr1.y,ptr2.x,ptr2.y);
-    printf("Distance of 2 point: %.2f\n",distance);
+    float  distance = sqrt(pow(ptr1.x - ptr2.x,2) + pow(ptr1.y - ptr2.y,2));
+    printf("Distance 2 point :%.2f\n",distance);
     return 0;
 }
